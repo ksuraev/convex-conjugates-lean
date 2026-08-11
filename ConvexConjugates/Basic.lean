7190cade0c5eb9314238a1571cbae75a5104a8fe
Kate Suraev <ksuraev@outlook.com>
2026-08-11 13:35:02 +1000

    prove LHS of 'or' in refine_2

--- ConvexConjugates/Basic.lean
+++ ConvexConjugates/Basic.lean
@@ -17,19 +17,21 @@ def epigraph : Set (E × ℝ) := {p | f p.1 ≤ p.2}
 
 /-- The Fenchel conjugate of `f : E → EReal`. -/
 noncomputable def fenchelConjugate (v : E) : EReal :=
-  ⨆ x : dom f, ⟪v, x⟫ - f x
+  ⨆ x : E, ⟪v, x⟫ - f x
 
 local postfix:max "∗" => fenchelConjugate -- https://lean-lang.org/doc/reference/latest/Notations-and-Macros/Custom-Operators/#operators
 
-theorem fenchel_young (v : E) (x : dom f) (h3 : f x ≠ ⊥) (h4: f x ≠ ⊤): ⟪v,x⟫ ≤ f x + f∗ v := by
+theorem fenchel_young (v x : E) (h3 : f x ≠ ⊥) (h4: f x ≠ ⊤): ⟪v,x⟫ ≤ f x + f∗ v := by
   have h : f∗ v ≥ ⟪v,x⟫ - f x := by
     exact le_iSup_iff.mpr fun b a ↦ a x
   have h2 : f x + f∗ v ≥ ⟪v,x⟫ - f x + f x := by
     refine (EReal.le_sub_iff_add_le ?_ ?_).mp ?_
     constructor
     · apply h3
-    · apply h4
-
+    · left
+      exact h4
+    · sorry
+  sorry
 
 
 lemma test (S : Set E) (x : S) : ⨆ y : S,  f y ≥ f x := by
