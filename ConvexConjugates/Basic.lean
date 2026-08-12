5ecd8b157b1cf4e9062d023d99427338ff7d72c6
Kate Suraev <ksuraev@outlook.com>
2026-08-12 10:10:44 +1000

    Changes: 2026-08-12 10:10:44

--- ConvexConjugates/Basic.lean
+++ ConvexConjugates/Basic.lean
@@ -19,6 +19,7 @@ noncomputable def fenchelConjugate (v : E) : EReal :=
 
 local postfix:max "∗" => fenchelConjugate
 
+/-- A function `f` is proper if its domain is nonempty. -/
 def IsProper : Prop := dom f ≠ ∅
 
 -- not great name but will do for now
@@ -34,14 +35,13 @@ lemma fenchelConjugate_not_neg_inf (v : E) : IsProper f → f∗ v ≠ ⊥ := by
   rw [EReal.lt_sub_iff_add_lt (Or.inl hb) (Or.inl ht)]
   exact compareOfLessAndEq_eq_lt.mp rfl
 
-
-
-theorem fenchel_young (v x : E) (h1 : f x ≠ ⊥) (h2 : f x ≠ ⊤) : ⟪v,x⟫ ≤ f x + f∗ v := by
-  have h : f∗ v ≥ ⟪v,x⟫ - f x := by
+/-- Fenchel-Young inequality for a proper function `f : E → EReal`. -/
+theorem fenchel_young (v x : E) (h1 : f x ≠ ⊥) (h2 : IsProper f) : ⟪v,x⟫ ≤ f x + f∗ v := by
+  have h3 : f∗ v ≥ ⟪v,x⟫ - f x := by
     exact le_iSup_iff.mpr fun b a ↦ a x
-  rw[ge_iff_le] at h
-  rw[EReal.sub_le_iff_le_add (Or.inl h1) (Or.inl h2)] at h
+  rw[ge_iff_le] at h3
+  rw [EReal.sub_le_iff_le_add (Or.inl h1) (Or.inr (fenchelConjugate_not_neg_inf f v h2))] at h3
   rw[add_comm]
-  exact h
+  exact h3
 
 #min_imports
