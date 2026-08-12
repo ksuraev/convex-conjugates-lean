29348bc1eb0cf565b5c040e3bd5317897e389e9f
Kate Suraev <ksuraev@outlook.com>
2026-08-12 13:00:04 +1000

    Changes: 2026-08-12 13:00:04

--- ConvexConjugates/Basic.lean
+++ ConvexConjugates/Basic.lean
@@ -35,6 +35,20 @@ lemma fenchelConjugate_not_neg_inf (v : E) : IsProper f → f∗ v ≠ ⊥ := by
   rw [EReal.lt_sub_iff_add_lt (Or.inl hb) (Or.inl ht)]
   exact compareOfLessAndEq_eq_lt.mp rfl
 
+-- not great name but will do for now
+lemma fenchelConjugate_not_neg_inf_2 (v : E) : IsProper f → f∗ v ≠ ⊥ := by
+  intro h
+  obtain ⟨x, ht, hb⟩ := Set.nonempty_iff_ne_empty.mpr h
+  rw [lt_top_iff_ne_top] at ht
+  rw [gt_iff_lt, bot_lt_iff_ne_bot] at hb
+  unfold fenchelConjugate
+  apply ne_of_gt
+  rw [@bot_lt_iSup]
+  use x
+  have h1 : (f x).toReal = f x := EReal.coe_toReal ht hb
+  rw [← h1]
+  exact compareOfLessAndEq_eq_lt.mp rfl
+
 /-- Fenchel-Young inequality for a proper function `f : E → EReal`. -/
 theorem fenchel_young (v x : E) (h1 : f x ≠ ⊥) (h2 : IsProper f) : ⟪v,x⟫ ≤ f x + f∗ v := by
   have h3 : f∗ v ≥ ⟪v,x⟫ - f x := by
