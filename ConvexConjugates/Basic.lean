7e0a64aba50516037618fd25750336dd0dd0db95
Kate Suraev <ksuraev@outlook.com>
2026-08-13 10:50:22 +1000

    Changes: 2026-08-13 10:50:22

--- ConvexConjugates/Basic.lean
+++ ConvexConjugates/Basic.lean
@@ -2,50 +2,44 @@ import Mathlib.Analysis.InnerProductSpace.Defs
 
 set_option linter.style.header false
 
+/-!
+# Convex Conjugates
+
+## TODO
+- Replace the definition of `fenchelConjugate` to apply on the dual space of `E`
+- Organise things better e.g. structure for EReal function, sections
+- Get rid of `h1` in `fenchel_young`
+-/
+
 local notation "⟪" x ", " y "⟫" => @inner ℝ _ _ x y
 
 variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E]
 variable (f : E → EReal)
 
+/-- The Fenchel conjugate of `f : E → EReal`. -/
+noncomputable def fenchelConjugate (v : E) : EReal := ⨆ x : E, ⟪v, x⟫ - f x
+
+/-- Notation for the Fenchel conjugate: `f∗`. -/
+local postfix:max "∗" => fenchelConjugate
+
 /-- The effective domain of `f`. -/
-def dom : Set E := {x : E | f x < ⊤ ∧ f x > ⊥}
+def dom : Set E := {x : E | f x ≠ ⊤ ∧ f x ≠ ⊥}
 
 /-- The epigraph of `f`. -/
-def epigraph : Set (E × ℝ) := {p | f p.1 ≤ p.2}
-
-/-- The Fenchel conjugate of `f : E → EReal`. -/
-noncomputable def fenchelConjugate (v : E) : EReal :=
-  ⨆ x : E, ⟪v, x⟫ - f x
-
-local postfix:max "∗" => fenchelConjugate
+def epi : Set (E × ℝ) := {p | f p.1 ≤ p.2}
 
 /-- A function `f` is proper if its domain is nonempty. -/
 def IsProper : Prop := dom f ≠ ∅
 
--- not great name but will do for now
-lemma fenchelConjugate_not_neg_inf (v : E) : IsProper f → f∗ v ≠ ⊥ := by
+/-- If a function `f` is proper, then its Fenchel conjugate is not `-∞`. -/
+lemma fenchelConjugate_ne_bot (v : E) : IsProper f → f∗ v ≠ ⊥ := by
   intro h
   obtain ⟨x, ht, hb⟩ := Set.nonempty_iff_ne_empty.mpr h
-  rw [lt_top_iff_ne_top] at ht
-  rw [gt_iff_lt, bot_lt_iff_ne_bot] at hb
-  unfold fenchelConjugate
-  apply ne_of_gt
-  rw [@bot_lt_iSup]
-  use x
-  rw [EReal.lt_sub_iff_add_lt (Or.inl hb) (Or.inl ht)]
-  exact compareOfLessAndEq_eq_lt.mp rfl
-
--- not great name but will do for now
-lemma fenchelConjugate_not_neg_inf_2 (v : E) : IsProper f → f∗ v ≠ ⊥ := by
-  intro h
-  obtain ⟨x, ht, hb⟩ := Set.nonempty_iff_ne_empty.mpr h
-  rw [lt_top_iff_ne_top] at ht
-  rw [gt_iff_lt, bot_lt_iff_ne_bot] at hb
-  unfold fenchelConjugate
-  apply ne_of_gt
-  rw [@bot_lt_iSup]
-  use x
   have h1 : (f x).toReal = f x := EReal.coe_toReal ht hb
+  unfold fenchelConjugate
+  apply ne_of_gt
+  rw [@bot_lt_iSup]
+  use x
   rw [← h1]
   exact compareOfLessAndEq_eq_lt.mp rfl
 
@@ -54,7 +48,7 @@ theorem fenchel_young (v x : E) (h1 : f x ≠ ⊥) (h2 : IsProper f) : ⟪v,x⟫
   have h3 : f∗ v ≥ ⟪v,x⟫ - f x := by
     exact le_iSup_iff.mpr fun b a ↦ a x
   rw[ge_iff_le] at h3
-  rw [EReal.sub_le_iff_le_add (Or.inl h1) (Or.inr (fenchelConjugate_not_neg_inf f v h2))] at h3
+  rw [EReal.sub_le_iff_le_add (Or.inl h1) (Or.inr (fenchelConjugate_ne_bot f v h2))] at h3
   rw[add_comm]
   exact h3
 
