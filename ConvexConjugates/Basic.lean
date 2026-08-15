c3099c4b8e97846b3897f882193d1d0ea7811712
Kate Suraev <ksuraev@outlook.com>
2026-08-15 13:53:12 +1000

    Changes: 2026-08-15 13:53:12

--- ConvexConjugates/Basic.lean
+++ ConvexConjugates/Basic.lean
@@ -15,22 +15,22 @@ local notation "⟪" x ", " y "⟫" => @inner ℝ _ _ x y
 variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E]
 variable (f : E → EReal)
 
-/-- The Fenchel conjugate of `f : E → EReal`. -/
-noncomputable def fenchelConjugate (v : E) : EReal := ⨆ x : E, ⟪v, x⟫ - f x
-
-/-- Notation for the Fenchel conjugate: `f∗`. -/
-local postfix:max "∗" => fenchelConjugate
-
-/-- The effective domain of `f`. -/
+-- The effective domain of `f`
 def dom : Set E := {x : E | f x ≠ ⊤ ∧ f x ≠ ⊥}
 
-/-- The epigraph of `f`. -/
+-- The epigraph of `f`
 def epi : Set (E × ℝ) := {p | f p.1 ≤ p.2}
 
-/-- A function `f` is proper if its domain is nonempty. -/
+-- A function `f` is proper if its domain is nonempty
 def IsProper : Prop := dom f ≠ ∅
 
-/-- If a function `f` is proper, then its Fenchel conjugate is not `-∞`. -/
+-- The Fenchel conjugate of `f : E → EReal`
+noncomputable def fenchelConjugate (v : E) : EReal := ⨆ x : E, ⟪v, x⟫ - f x
+
+-- Notation for the Fenchel conjugate: `f∗`
+local postfix:max "∗" => fenchelConjugate
+
+-- If a function `f` is proper, then its Fenchel conjugate is not `-∞`
 lemma fenchelConjugate_ne_bot (v : E) : IsProper f → f∗ v ≠ ⊥ := by
   intro h
   obtain ⟨x, ht, hb⟩ := Set.nonempty_iff_ne_empty.mpr h
@@ -42,7 +42,7 @@ lemma fenchelConjugate_ne_bot (v : E) : IsProper f → f∗ v ≠ ⊥ := by
   rw [← h1]
   exact compareOfLessAndEq_eq_lt.mp rfl
 
-/-- Fenchel-Young inequality for a proper function `f : E → EReal`. -/
+-- Fenchel-Young inequality for a proper function `f : E → EReal`
 theorem fenchel_young (v x : E) (h1 : f x ≠ ⊥) (h2 : IsProper f) : ⟪v,x⟫ ≤ f x + f∗ v := by
   have h3 : f∗ v ≥ ⟪v,x⟫ - f x := by
     exact le_iSup_iff.mpr fun b a ↦ a x
@@ -51,9 +51,10 @@ theorem fenchel_young (v x : E) (h1 : f x ≠ ⊥) (h2 : IsProper f) : ⟪v,x⟫
   rw[add_comm]
   exact h3
 
-/-- The Fenchel biconjugate of `f : E → EReal`. -/
+-- The Fenchel biconjugate of `f : E → EReal`
 noncomputable def fenchelBiconjugate (x : E) : EReal := ⨆ v : E, ⟪v, x⟫ - f∗ v
 
+-- Notation for the Fenchel biconjugate: `f∗∗`
 local postfix:max "∗∗" => fenchelBiconjugate
 
 -- Assuming we make the same assumptions as in the Fenchel-Young inequality?
